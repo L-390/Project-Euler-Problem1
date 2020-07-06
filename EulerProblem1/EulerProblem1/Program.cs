aa718156321dfcbf2d63cf4fadf7ef4d2cb5be18
Liam <67001127+L-390@users.noreply.github.com>
2020-07-06 09:50:24 +0200

    Add files via upload

---
+++ EulerProblem1/EulerProblem1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EulerProblem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int sum = 0;
+                    
+            while(number <= 1000)
+            {
+                number += 1;
+
+                if((number % 3) == 0 || (number % 5) == 0)
+                {
+                    Console.WriteLine(number);
+                    sum += number;
+                }
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
